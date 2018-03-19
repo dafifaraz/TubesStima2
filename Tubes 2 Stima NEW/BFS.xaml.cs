e4ab6ad5c08dbbfe480e4deccf647a91e0376758
dafifaraz <30387504+dafifaraz@users.noreply.github.com>
2018-03-19 19:47:31 +0700

    BFS check

--- Tubes 2 Stima NEW/BFS.xaml.cs
+++ Tubes 2 Stima NEW/BFS.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +11,13 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Management;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace Tubes_2_Stima_NEW
 {
@@ -27,6 +34,21 @@ namespace Tubes_2_Stima_NEW
         {
             public string _NamaMatKul;
             public List<string> _PreRequisite;
+        }
+        public static uint currentsp, Maxsp;
+        public void CPUSpeed()
+        {
+            using (ManagementObject Mo = new ManagementObject("Win32_Processor.DeviceID='CPU0'"))
+            {
+                currentsp = (uint)(Mo["CurrentClockSpeed"]);
+                Maxsp = (uint)(Mo["MaxClockSpeed"]);
+            }
+        }
+        const int MAX_SEMESTER = 10;
+        public struct _SemesterX
+        {
+            public int _X; //SEMESTER KE-X
+            public List<string> _NamaMatKul; //NAMANYA, CONTOH "C1"
         }
 
     }
